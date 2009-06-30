0e4873fd61b54e53bac03b27a41973a30df9e51b
shlomi.noach <shlomi.noach@0dce6042-c447-11dd-b192-8d970c37f332>
2009-06-30 06:24:43 +0000

    Revision 96 - dist scripts updated

--- openarkkit/py-dist-cfg/setup.py
+++ openarkkit/py-dist-cfg/setup.py
@@ -6,7 +6,7 @@ setup(
     author="Shlomi Noach",
     author_email="shlomi@code.openark.org",
     url="http://code.openark.org/forge/openark-kit",
-    version="84",
+    version="96",
     requires=["MySQLdb"],
     packages=[""],
     package_dir={"": "scripts"},
