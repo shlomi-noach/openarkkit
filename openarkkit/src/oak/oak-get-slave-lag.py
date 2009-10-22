8efc4b6576c69857cd42503af333e549f908fd57
shlomi.noach <shlomi.noach@0dce6042-c447-11dd-b192-8d970c37f332>
2009-10-22 19:06:16 +0000

    added: oak-get-slave-lag

---
+++ openarkkit/src/oak/oak-get-slave-lag.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+
+#
+# Show slave lag in seconds; possible exit with error code if lag is to large.
+#
+# Released under the BSD license
+#
+# Copyright (c) 2008, Shlomi Noach
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#     * Neither the name of the organization nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+import getpass
+import MySQLdb
+import sys
+from optparse import OptionParser
+
+def parse_options():
+    usage = "usage: oak-get-slave-lag [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-u", "--user", dest="user", default="", help="MySQL user")
+    parser.add_option("-H", "--host", dest="host", default="localhost", help="MySQL host (default: localhost)")
+    parser.add_option("-p", "--password", dest="password", default="", help="MySQL password")
+    parser.add_option("--ask-pass", action="store_true", dest="prompt_password", help="Prompt for password")
+    parser.add_option("-P", "--port", dest="port", type="int", default="3306", help="TCP/IP port (default: 3306)")
+    parser.add_option("-S", "--socket", dest="socket", default="/var/run/mysqld/mysql.sock", help="MySQL socket file. Only applies when host is localhost")
+    parser.add_option("", "--defaults-file", dest="defaults_file", default="", help="Read from MySQL configuration file. Overrides all other options")
+    parser.add_option("-e", "--error-if-more-than-seconds", dest="error_if_more_than_seconds", type="int", default=None, help="Return with error exit code if slave lag is more than given number of seconds (default: disabled)")
+    return parser.parse_args()
+
+
+def verbose(message):
+    if options.verbose:
+        print "-- %s" % message
+
+def print_error(message):
+    print >>sys.stderr, message
+
+def open_connection():
+    if options.defaults_file:
+        conn = MySQLdb.connect(
+            read_default_file = options.defaults_file)
+    else:
+        if options.prompt_password:
+            password=getpass.getpass()
+        else:
+            password=options.password
+        conn = MySQLdb.connect(
+            host = options.host,
+            user = options.user,
+            passwd = password,
+            port = options.port,
+            unix_socket = options.socket)
+    return conn;
+
+def act_query(query):
+    """
+    Run the given query, commit changes
+    """
+    connection = conn
+    cursor = connection.cursor()
+    num_affected_rows = cursor.execute(query)
+    cursor.close()
+    connection.commit()
+    return num_affected_rows
+
+
+def get_row(query):
+    connection = conn
+    cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(query)
+    row = cursor.fetchone()
+
+    cursor.close()
+    return row
+
+
+def get_rows(query):
+    connection = conn
+    cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    cursor.close()
+    return rows
+
+
+def get_slave_delay_seconds():
+    slave_status = get_row("SHOW SLAVE STATUS")
+    if slave_status is None:
+        return None
+    seconds_behind_master_value = slave_status["Seconds_Behind_Master"]
+    if seconds_behind_master_value is None:
+        return None
+    return int(seconds_behind_master_value)
+
+
+def get_slave_delay():
+    seconds_behind_master = get_slave_delay_seconds()
+    if options.error_if_more_than_seconds is None:
+        print seconds_behind_master
+    elif seconds_behind_master is None or seconds_behind_master > options.error_if_more_than_seconds:
+        exit_with_error(seconds_behind_master)
+    else:    
+        print seconds_behind_master
+
+
+def exit_with_error(error_message):
+    """
+    Notify and exit.
+    """
+    print_error(error_message)
+    exit(1)
+
+
+try:
+    try:
+        conn = None
+        reuse_conn = True
+        (options, args) = parse_options()
+
+        conn = open_connection()
+        get_slave_delay()
+    except Exception, err:
+        print err
+finally:
+    if conn:
+        conn.close()
